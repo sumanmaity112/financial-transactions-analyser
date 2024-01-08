cbf754bf5937c53f3d14c048ef34455cc1c2edd5
Suman Maity <suman.maity112@gmail.com>
2024-01-08 18:48:08 +0530

    Use center alignment for header

--- src/components/ApplicationHeader.vue
+++ src/components/ApplicationHeader.vue
@@ -8,7 +8,7 @@ export default {
   <v-app-bar elevation="5">
     <v-app-bar-nav-icon icon="mdi-home" href="#" />
     <v-spacer />
-    <v-app-bar-title class="font-weight-bold" text="Finanza" />
+    <v-app-bar-title class="font-weight-bold text-center" text="Finanza" />
     <v-spacer />
     <v-btn
       target="_blank"
